8cdec2cdc9eddeeeaa9a0a187f5e28ec26970141
GabrielBertasso <160244713+GabrielBertasso@users.noreply.github.com>
2024-11-22 08:26:57 -0300

    carga_js-tp-var-fn

---
+++ type-number.js
@@ -0,0 +1,13 @@
+//tipo Number
+const meuNumero = 3;
+const primeiroNumero = 1;
+const segundoNumero = 2;
+const operacaoMatematica = primeiroNumero-segundoNumero;
+console.log (operacaoMatematica)
+// ponto flutuante 
+const pontoFlutuanteSemzero = 3.3;
+const novaOperacao = primeiroNumero/numeropontoFlutuante;
+console.log(novaOperacao)
+//Na Not.Number (não é um numero)
+const alura = "Alura";
+console.log(Alura*primeiroNumero)
